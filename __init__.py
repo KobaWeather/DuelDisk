2ce5ffd020b0cc7573c0274cef47f2990c1ebc83
KobaWeather <weather.l.a.c.g.j.kuni@gmail.com>
2016-06-24 10:31:28 +0900

    スクリプト及びデータベースの検索範囲を広げ、アニメ版などのカードについても引っかかるように変更。 それに伴い、検索部分を関数化

--- __init__.py
+++ __init__.py
@@ -4,6 +4,10 @@
 from PIL import Image
 import sqlite3 as sq
 import os
+import sys
+
+argvs = sys.argv
+argc = len(argvs)
 
 #resize用
 SIZE = 56
@@ -13,6 +17,44 @@ box = (29,55,141,167)
 #local_placeはYgoproが置かれているディレクトリを指定
 local_place = "/Volumes/NO NAME/"
 place = local_place+"Ygopro/"
+
+def create_deck():
+	if argc >= 3:
+		deck_name = place+"deck/"+argvs[-1]
+		#デッキの取得
+		deck = [int(i.strip("\n")) for i in open(deck_name,"r").readlines() if not("e" in i or "a" in i)]
+		#重複削除
+		ones = list(set(deck))
+		#比較データの取得
+		f = open("data.txt","r")
+		data = [i.split(",") for i in f.readlines() if int(i.split(",")[0]) in ones]
+		f.close()
+	
+	else:#デッキ縛りしない場合はこっち
+		f = open("data.txt","r")
+		data = [i.split(",") for i in f.readlines() if int(i.split(",")[0])]
+		f.close()
+		
+	return data
+
+#スクリプトデータの取得
+def search_lua(lua):
+	try:
+		f = open(lua,"r")
+		card = f.readlines()
+		f.close()
+		return card
+	except IOError:
+		return 0
+
+#データベース検索
+def search_cdb(cdb,decide_card):
+	con = sq.connect(cdb)
+	c = con.cursor()
+	data = [i for i in c.execute(u"select * from datas where id = ?",(decide_card,))]
+	text = [i for i in c.execute(u"select name,desc from texts where id = ?",(decide_card,))]
+	con.close()
+	return data,text
 
 #Average Hash法
 def avhash(im):
--- recog.py
+++ recog.py
@@ -3,82 +3,62 @@
 
 from __init__ import *
 
-import sys
-
-argvs = sys.argv
-argc = len(argvs)
-
-if argc >= 3:
-	deck_name = place+"deck/"+argvs[-1]
-	#デッキの取得
-	deck = [int(i.strip("\n")) for i in open(deck_name,"r").readlines() if not("e" in i or "a" in i)]
-	#重複削除
-	ones = list(set(deck))
-	#比較データの取得
-	f = open("data.txt","r")
-	data = [i.split(",") for i in f.readlines() if int(i.split(",")[0]) in ones]
-	f.close()
-
-else:
-	f = open("data.txt","r")
-	data = [i.split(",") for i in f.readlines() if int(i.split(",")[0])]#デッキ縛りしない場合はこっち
-	f.close()
+#比較用データの取得
+data = create_deck()
 
 #調べたい画像の取得
 h = Image.open(argvs[1]).convert("L")
-par = float(h.size[0])/float(h.size[1])
-if 0.6 < par < 0.7:
-	h = h.resize((177,254),Image.ANTIALIAS).crop(box)
+h = h.resize((177,254),Image.ANTIALIAS).crop(box)
 h = h.resize((SIZE,SIZE))
+#h.save("gray.png")
+#h.convert("1").save("one.png")
 h = avhash(h)
+
+
 	
 #ハミング距離の測定
 seg = [(i[0],hamming(int(i[1]),h)) for i in data]
+
+for f, ham in sorted(seg, key=lambda i: i[1])[:5]:
+	print str(f)+":"+str(ham)
 	
 #順位設定
 card = [f for f, ham in sorted(seg, key=lambda i: i[1])[:5]]
 
+#カードの決定
 decide_card = card[0]
 
 print "\nカード番号:\n",decide_card
 Image.open(place+"pics/"+decide_card+".jpg").show()
+#Image.open(place+"pics/"+decide_card+".jpg").save("resullt.png")
 
 #スクリプトデータの取得
-try:
-	f = open(place+"script/c"+decide_card+".lua","r")
-	card = f.readlines()
-	f.close()
-except IOError:#通常のスクリプトにない場合は別フォルダに入っている
-	f = open(place+"expansions/live/script/c"+decide_card+".lua","r")
-	card = f.readlines()
-	f.close()
+lua = [place+"script/c"+decide_card+".lua",place+"expansions/live/script/c"+decide_card+".lua",place+"expansions/script/c"+decide_card+".lua"]
+
+for i in lua:
+	card = search_lua(i)
+	if card != 0:
+		break
 
 #カードの表示
 #print card[0].strip("--")
 
 #データベースへ接続->データ抽出
 #英語版で探したい場合は日本語版をコメントアウト(#)
-cdb = place + "cards.cdb"#英語版
-cdb = place+"/0x1339-20160207-nopics/cards.cdb"#日本語版
+c = place+"/0x1339-20160207-nopics/cards.cdb"#日本語版
+c = place + "cards.cdb"#英語版
 
-con = sq.connect(cdb)
-c = con.cursor()
-data = [i for i in c.execute(u"select * from datas where id = ?",(decide_card,))]
-#データ取得完了？？
-text = [i for i in c.execute(u"select name,desc from texts where id = ?",(decide_card,))]
+cdb_place = place+"/expansions/live/"
+cdb = [c,place+"/expansions/cards-tf.cdb"]+[cdb_place+base for base in os.listdir(cdb_place) if ".cdb" in base]
 
-con.close()
-
+data = []
 #通常のcdbで見つからなかった時はliveフォルダ内のcdbから検索
-cdb = place+"/expansions/live/"
-for base in os.listdir(cdb):
-	if len(data) == 0 and ".cdb" in base:
-		con = sq.connect(cdb+base)
-		c = con.cursor()
-		data = [i for i in c.execute(u"select * from datas where id = ?",(decide_card,))]
-		text = [i for i in c.execute(u"select name,desc from texts where id = ?",(decide_card,))]
-		con.close()
-
+for base in cdb:
+	if len(data) == 0:
+		data,text = search_cdb(base,decide_card)
+	else:
+		break
+		
 #カード名と効果を表示
 print "\nカード名:\n",text[0][0]
 print "\nカード効果:\n",text[0][1]
